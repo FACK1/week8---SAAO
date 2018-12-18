a80f82d917b6f74224d7f62fdb38f4ff8a038d76
orjwan98 <orjwan98@hotmail.com>
2018-12-18 15:22:36 +0200

    made getQuery and postQuery yay! #15

---
+++ src/queries/getImg.js
@@ -0,0 +1,12 @@
+const db_connection = require("../database/db_connection");
+
+const getImg = cb => {
+  db_connection.query("select * from  image", (err, data) => {
+    if (err) {
+      cb(err);
+    } else {
+      cb(null, data.rows);
+    }
+  });
+};
+module.exports = getImg;
