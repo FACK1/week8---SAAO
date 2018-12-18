3a792095e00826c0234eea67350056b23bd85635
Amin <ayman201266@hotmail.com>
2018-12-18 10:37:19 +0200

    create the connection of database #6

---
+++ src/database/db_connection.js
@@ -0,0 +1,14 @@
+const { Pool } = require('pg');
+require('env2')('config.env');
+
+const { DATABASE_URL } = process.env;
+
+if (!DATABASE_URL) {
+  throw Error('No database URL');
+}
+
+const option = {
+  connectionString: DATABASE_URL,
+};
+
+module.exports = new Pool(option);
