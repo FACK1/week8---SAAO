ddbaacb8ef8e7986bb43944780611d8d8b57477f
safaaamro <safaaamro072@gmail.com>
2018-12-19 15:50:37 +0200

    return the message errors #23

--- src/validation/validate.js
+++ src/validation/validate.js
@@ -4,7 +4,15 @@ const validate = (validateCheck) => (req, res, next) => {
   const validateStatus = Joi.validate(req.body, validateCheck, { abortEarly: false });
 
   if (validateStatus.error){
-    req.error=validateStatus.error;
+   console.log(validateStatus.error.details)
+    req.error=validateStatus.error.details.map(error =>{
+    if (error.context.key ==='img_url') {
+    return   error.message='Wrong url image please enter vaild one ';
+  }else if (error.context.key=='description') {
+    return   error.message=' unreadable description of image please enter vaild one';
+
+  }
+    });
     next();
   }
   else
